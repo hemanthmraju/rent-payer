ace23cbb5f214f879c62a5a46e12abce6aa3b800
hemanthmraju <kumarhemanth.cs@gmail.com>
2014-11-13 10:42:53 +0530

    my first commit

---
+++ config/initializers/database_config.rb
@@ -0,0 +1,5 @@
+
+  # Set up database initializer 'newplan_db'
+    ENV[Rails.env.to_s+'_db'] = 'rent_payer'
+    ENV[Rails.env.to_s+'_username'] = 'root'
+    ENV[Rails.env.to_s+'_password'] = 'admin'
