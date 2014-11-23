03d3d58898695c3106c9554e81ab05281f8aa31b
hemanth <kumarhemanth.cs@gmail.com>
2014-11-23 15:16:40 +0530

    all changes upadetd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :layout
+
+  private
+
+  def layout
+    # only turn it off for login pages:
+    p "lau"*20
+    p (is_a?(Devise::SessionsController) || is_a?(Devise::RegistrationsController))
+    (is_a?(Devise::SessionsController) || is_a?(Devise::RegistrationsController)) ? false : "application"
+    # or turn layout off for every devise controller:
+    #devise_controller? && "application"
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
-	before_filter :authenticate_user!
-  
+	#before_filter :authenticate_user!
+  	skip_before_filter :authenticate_user, :only => [:index]
   def index
   	
   end
